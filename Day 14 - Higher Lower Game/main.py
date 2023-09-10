d9004e0357e3d4035b6341f714f8182940516a71
JPBradburn <140695410+JPBradburn@users.noreply.github.com>
2023-09-10 11:41:53 +1000

    day 14

---
+++ Day 14 - Higher Lower Game/main.py
@@ -0,0 +1,28 @@
+import random
+
+import art
+import  game_data
+
+print(art.logo)
+
+score=0
+
+def turn():
+    A = random.choice(game_data.data)
+    B = random.choice(game_data.data)
+    print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}")
+    print(art.vs)
+    print(f"Compare B: {B['name']}, a {B['description']} from {B['country']}")
+
+    if game_data.data.index(eval(input("Who has more followers? Type 'A' or 'B': ").upper())) == min(game_data.data.index(A), game_data.data.index(B)):
+        for i in range(50):
+            print()
+        print(art.logo)
+        print("You're right! Current score: " + str(score))
+        turn()
+    else:
+        print(art.logo)
+        print("Sorry, that's wrong. Final score: " + str(score))
+        if input("Do you want to play again? ('Y' / 'N'): ").lower() == "y":
+            turn()
+turn()
