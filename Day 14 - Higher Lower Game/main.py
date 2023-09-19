02f54916302e8ca20f2cd28aa0b32d7bdf24e6cd
JPBradburn <140695410+JPBradburn@users.noreply.github.com>
2023-09-19 15:02:05 +1000

    day 14 fix

--- Day 14 - Higher Lower Game/main.py
+++ Day 14 - Higher Lower Game/main.py
@@ -1,28 +1,32 @@
 import random
 
 import art
-import  game_data
+import game_data
 
 print(art.logo)
 
 score=0
 
-def turn():
-    A = random.choice(game_data.data)
+def turn(A):
+    global score
+
     B = random.choice(game_data.data)
+    print()
     print(f"Compare A: {A['name']}, a {A['description']} from {A['country']}")
     print(art.vs)
     print(f"Compare B: {B['name']}, a {B['description']} from {B['country']}")
+    print()
 
     if game_data.data.index(eval(input("Who has more followers? Type 'A' or 'B': ").upper())) == min(game_data.data.index(A), game_data.data.index(B)):
         for i in range(50):
             print()
         print(art.logo)
+        score += 1
         print("You're right! Current score: " + str(score))
-        turn()
+        turn(B)
     else:
         print(art.logo)
         print("Sorry, that's wrong. Final score: " + str(score))
         if input("Do you want to play again? ('Y' / 'N'): ").lower() == "y":
             turn()
-turn()
+turn(random.choice(game_data.data))
